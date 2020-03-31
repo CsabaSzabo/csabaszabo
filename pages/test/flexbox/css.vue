b09daaaa1dc251bbbe84976100afc6eac87fe97c
Csaba Szabo <sz.csabi.3@gmail.com>
2020-03-31 09:12:40 +0200

    flexbox pages

---
+++ pages/test/flexbox/css.vue
@@ -0,0 +1,255 @@
+<template>
+  <!-- 
+    This flexbox page is a learning page about Flexbox CSS layout
+   -->
+  <div>
+    <p>
+      A flexbox example page with
+      <a href="https://www.w3schools.com/css/css3_flexbox.asp" target="_blank">pure CSS code</a>
+    </p>
+    
+    <h2>Container 1 <code>flex-direction</code></h2>
+    <div class="cont-column">
+      <h3>Container <code>flex-direction: column</code></h3>
+      <div class="cont cont-column">
+        <div class="item">item1</div>
+        <div class="item">item2</div>
+      </div>
+      <h3>Container <code>flex-direction: row</code></h3>
+      <div class="cont">
+        <div class="item item1">item1 order=2</div>
+        <div class="item item2">item2 order=1</div>
+      </div>
+    </div>
+
+    <h2>Container 2 <code>wraps</code></h2>
+    <div class="cont-column">
+      <h3>Container CSS <code>flex-wrap: no-wrap</code> (default)</h3>
+      <div class="cont">
+        <div class="item item-long" v-for="index in Array.from(Array(10).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      <h3>Container CSS <code>flex-wrap: wrap</code></h3>
+      <div class="cont cont-wrap">
+        <div class="item item-long" v-for="index in Array.from(Array(10).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      <h3>Container CSS <code>flex-wrap: wrap-reverse</code></h3>
+      <div class="cont cont-wrap-reverse">
+        <div class="item item-long" v-for="index in Array.from(Array(10).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+    </div>
+
+    <h2>Container 3 <code>justify-content</code></h2>
+    <div class="cont-column">  
+      <h3>Container CSS <code>justify-content: flex-start</code> (default)</h3>
+      <div class="cont cont-flex-start">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+
+      <h3>Container CSS <code>justify-content: flex-end</code></h3>
+      <div class="cont cont-flex-end">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      
+      <h3>Container CSS <code>justify-content: center</code></h3>
+      <div class="cont cont-center">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      
+      <h3>Container CSS <code>justify-content: space-between</code></h3>
+      <div class="cont cont-space-between">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      
+      <h3>Container CSS <code>justify-content: space-around</code></h3>
+      <div class="cont cont-space-around">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      
+      <h3>Container CSS <code>justify-content: space-evenly</code></h3>
+      <div class="cont cont-space-evenly">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+    </div>
+
+    <h2>Container 4 <code>align-items</code></h2>
+    <div class="cont-column">
+      <div
+        v-for="aiConfig in [
+          {class: '', name: 'stretch', default: true},
+          {class: 'cont-ai-start', name: 'start', default: false},
+          {class: 'cont-ai-end', name: 'end', default: false},
+          {class: 'cont-ai-center', name: 'center', default: false},
+          {class: 'cont-ai-baseline', name: 'baseline', default: false}]"
+        :key="aiConfig.class"
+      >
+        <h3>
+          <span>Container CSS </span>
+          <code>align-items: {{ aiConfig.name }}</code>
+          <span v-if="aiConfig.default"> (default)</span>
+        </h3>
+        <div
+          class="cont"
+          :class="aiConfig.class"
+        >
+          <div
+            v-for="index in Array.from(Array(5).keys())"
+            :key="index"
+            class="item item-long"
+            :class="`item-height-${index}`"
+          >
+            item{{ index }}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <h2>Container 5 <code>align-content</code></h2>
+    <div class="cont-column">
+      <div
+        v-for="acConfig in [
+          {class: 'cont-ac-stretch', name: 'stretch', default: true},
+          {class: 'cont-ac-start', name: 'start', default: false},
+          {class: 'cont-ac-end', name: 'end', default: false},
+          {class: 'cont-ac-center', name: 'center', default: false},
+          {class: 'cont-ac-space-between', name: 'space-between', default: false},
+          {class: 'cont-ac-space-around', name: 'space-around', default: false},
+          {class: 'cont-ac-space-evenly', name: 'space-evenly', default: false}]"
+        :key="acConfig.class"
+      >
+        <h3>
+          <span>Container CSS</span>
+          <code>align-content: {{ acConfig.name }}</code>
+          <span v-if="acConfig.default">(default)</span>
+        </h3>
+        <div
+          class="cont cont-wrap cont-ac"
+          :class="acConfig.class"
+        >
+          <div
+            v-for="index in Array.from(Array(15).keys())"
+            :key="index"
+            class="item item-long"
+          >
+            item{{ index }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '~/assets/css/pages/test/flexbox.scss';
+
+.cont {
+  display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
+  display: -ms-flexbox;      /* TWEENER - IE 10 */
+  display: -webkit-flex;     /* NEW - Chrome */
+  display: flex;
+}
+.cont-column {
+  -webkit-flex-direction: column;   /* Chrome, Edge, Safari */
+  -ms-flex-direction: column;       /* IE 10 */
+  flex-direction: column;
+}
+
+.cont-wrap {
+  $wrap: wrap;
+  -webkit-flex-wrap: $wrap;
+  flex-wrap: $wrap;
+}
+.cont-wrap-reverse {
+  $wrap: wrap-reverse;
+  -webkit-flex-wrap: $wrap;
+  flex-wrap: $wrap;
+}
+
+.cont-flex-start {
+  -webkit-justify-content: flex-start;
+  justify-content: flex-start;
+}
+.cont-flex-end {
+  -webkit-justify-content: flex-end;
+  justify-content: flex-end;
+}
+.cont-center {
+  -webkit-justify-content: center;
+  justify-content: center;
+}
+.cont-space-between {
+  -webkit-justify-content: space-between;
+  justify-content: space-between;
+}
+.cont-space-around {
+  -webkit-justify-content: space-around;
+  justify-content: space-around;
+}
+.cont-space-evenly {
+  -webkit-justify-content: space-evenly;
+  justify-content: space-evenly;
+}
+
+.cont-ai-start {
+  -webkit-align-items: start;
+  align-items: start;
+}
+.cont-ai-end {
+  -webkit-align-items: end;
+  align-items: end;
+}
+.cont-ai-center {
+  -webkit-align-items: center;
+  align-items: center;
+}
+.cont-ai-baseline {
+  -webkit-align-items: baseline;
+  align-items: baseline;
+}
+
+.cont-ac-stretch {
+  -webkit-align-content: stretch;
+  align-content: stretch;
+}
+.cont-ac-start {
+  -webkit-align-content: start;
+  align-content: start;
+}
+.cont-ac-end {
+  -webkit-align-content: end;
+  align-content: end;
+}
+.cont-ac-center {
+  -webkit-align-content: center;
+  align-content: center;
+}
+.cont-ac-space-between {
+  -webkit-align-content: space-between;
+  align-content: space-between;
+}
+.cont-ac-space-around {
+  -webkit-align-content: space-around;
+  align-content: space-around;
+}
+.cont-ac-space-evenly {
+  -webkit-align-content: space-evenly;
+  align-content: space-evenly;
+}
+</style>
---
+++ pages/test/flexbox/vuetify.vue
@@ -0,0 +1,157 @@
+<template>
+  <!-- 
+    This flexbox page is a learning page about Flexbox CSS layout
+   -->
+  <div>
+    <p>
+      A flexbox example page with
+      <a hreaf="https://vuetifyjs.com/en/styles/flex/#enabling-flexbox" target="_blank">Vuetify flexbox classes</a>
+    </p>
+    
+    <h2>Container 1 <code>flex-direction</code></h2>
+    <div class="flex-column">
+      <h3>Container <code>d-flex flex-column</code></h3>
+      <div class="d-flex flex-column">
+        <div class="item">item1</div>
+        <div class="item">item2</div>
+      </div>
+
+      <h3>Container <code>d-flex</code></h3>
+      <div class="d-flex">
+        <div class="item item1">item1 order=2</div>
+        <div class="item item2">item2 order=1</div>
+      </div>
+    </div>
+
+    <h2>Container 2 <code>wraps</code></h2>
+    <div class="flex-column">
+      <h3>Container class <code>no-wrap</code> (default)</h3>
+      <div class="d-flex">
+        <div class="item item-long" v-for="index in Array.from(Array(10).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      <h3>Container class <code>flex-wrap</code></h3>
+      <div class="d-flex flex-wrap">
+        <div class="item item-long" v-for="index in Array.from(Array(10).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      <h3>Container class <code>flex-wrap-reverse</code></h3>
+      <div class="d-flex flex-wrap-reverse">
+        <div class="item item-long" v-for="index in Array.from(Array(10).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+    </div>
+
+    <h2>Container 3 <code>justify-content</code></h2>
+    <div class="flex-column">  
+      <h3>Container class <code>justify-start</code> (default)</h3>
+      <div class="d-flex justify-start">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+
+      <h3>Container class <code>justify-end</code></h3>
+      <div class="d-flex justify-end">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      
+      <h3>Container class <code>justify-center</code></h3>
+      <div class="d-flex justify-center">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      
+      <h3>Container class <code>justify-space-between</code></h3>
+      <div class="d-flex justify-space-between">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      
+      <h3>Container class <code>justify-space-around</code></h3>
+      <div class="d-flex justify-space-around">
+        <div class="item item-long" v-for="index in Array.from(Array(3).keys())" :key="index">
+          item{{ index }}
+        </div>
+      </div>
+      
+      <h3>Container space-evenly</h3>
+      <p>Not supported in Vuetify.</p>
+    </div>
+
+    <h2>Container 4 <code>align-items</code></h2>
+    <div class="flex-column">
+      <div
+        v-for="aiConfig in [
+          {class: 'align-stretch', name: 'align-stretch', default: true},
+          {class: 'align-start', name: 'align-start', default: false},
+          {class: 'align-end', name: 'align-end', default: false},
+          {class: 'align-center', name: 'align-center', default: false},
+          {class: 'align-baseline', name: 'align-baseline', default: false}]"
+        :key="aiConfig.class"
+      >
+        <h3>
+          Container class 
+          <code>{{ aiConfig.class }}</code>
+          <span v-if="aiConfig.default"> (default)</span>
+        </h3>
+        <div
+          class="d-flex"
+          :class="aiConfig.class"
+        >
+          <div
+            v-for="index in Array.from(Array(5).keys())"
+            :key="index"
+            class="item item-long"
+            :class="`item-height-${index}`"
+          >
+            item{{ index }}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <h2>Container 5 <code>align-content</code></h2>
+    <div class="flex-column">
+      <div
+        v-for="acConfig in [
+          {class: 'align-content-stretch', name: 'align-content-stretch (default)', default: true},
+          {class: 'align-content-start', name: 'align-content-start', default: false},
+          {class: 'align-content-end', name: 'align-content-end', default: false},
+          {class: 'align-content-center', name: 'align-content-center', default: false},
+          {class: 'align-content-space-between', name: 'align-content-space-between', default: false},
+          {class: 'align-content-space-around', name: 'align-content-space-around', default: false}]"
+        :key="acConfig.class"
+      >
+        <h3>
+          <span>Container class</span>
+          <code>{{ acConfig.class }}</code>
+          <span v-if="acConfig.default"> (default)</span>
+        </h3>
+        <div
+          class="d-flex flex-wrap cont-ac"
+          :class="acConfig.class"
+        >
+          <div
+            v-for="index in Array.from(Array(15).keys())"
+            :key="index"
+            class="item item-long"
+          >
+            item{{ index }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '~/assets/css/pages/test/flexbox.scss';
+</style>
